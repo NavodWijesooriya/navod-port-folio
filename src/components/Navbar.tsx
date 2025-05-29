1dfbc2e7c176b1f82be79a4d41716fd0940d2bdc
navodwijesooriya <navodtwijesooriya@gmail.com>
2025-05-29 17:20:36 +0530

    add right link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -73,7 +73,7 @@ const Navbar = () => {
             </Link>
           ))}
           <Link
-            href="#get-started"
+            href="/contact"
             className="inline-block px-8 py-2 bg-green-600 text-white font-bold rounded-md shadow-lg hover:bg-green-700 transition duration-300 glow-effect font-mono"
           >
             Hire Me
